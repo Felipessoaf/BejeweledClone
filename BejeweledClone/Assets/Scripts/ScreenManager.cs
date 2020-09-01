b091a9ed6a13f2551840874e6318e631916d4e91
Felipe Pessoa <felipessoaf@hotmail.com>
2020-08-31 21:16:51 -0300

    screen manager

---
+++ BejeweledClone/Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour
+{
+    #region Variables
+    [Header("References")]
+    public GameObject MenuScreen;
+    public GameObject GameScreen;
+    public GameObject GameOverScreen;
+    #endregion
+
+    void Start()
+    {
+        MenuScreen.SetActive(true);
+        GameScreen.SetActive(true);
+        GameOverScreen.SetActive(true);
+    }
+
+    IEnumerator DeactivateScreens()
+    {
+        yield return new WaitForEndOfFrame();
+        GameScreen.SetActive(false);
+        GameOverScreen.SetActive(false);
+    }
+}
